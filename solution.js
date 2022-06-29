3d4ed0c4b19e36c3ef7337e6e91ff8552547946e
Your Name <tpunchihewage@gmail.com>
2022-06-29 15:14:24 +0200

    modified files added

---
+++ solution.js
@@ -0,0 +1,36 @@
+// Variables-1
+
+const apples = 20;
+const oranges = 30;
+
+console.log(apples == oranges);
+
+// Variable-2
+
+console.log(apples != oranges);
+
+// Variable-3
+
+console.log(apples > oranges);
+
+// Variables-4
+
+console.log(apples <= oranges);
+
+// Variables-5
+
+console.log(oranges > apples);
+
+// Variable-6
+
+const mangos = 5;
+
+console.log(mangos * apples == mangos * oranges);
+console.log(mangos / apples == mangos / oranges);
+console.log((mangos == apples) == oranges);
+
+console.log(apples / mangos == oranges / mangos);
+
+console.log(mangos + apples == oranges - mangos);
+
+// Variable-7
